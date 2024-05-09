37099983d08f1c31b425696722b5a778748cc91d
Andres Pichimata <afpichimata@hotmail.com>
2024-05-08 19:53:32 -0500

    setting up github actions cicd

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
+            <h2>Fibonacci Server</h2>
             <Link to="/">Home</Link>
             <Link to="/">Other Page</Link>
           </div>
